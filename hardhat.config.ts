ca90f5d0824b47dea2e776b2ad8725a22f5457b4
yonlu <lss.emailbox@gmail.com>
2021-12-18 23:05:35 -0500

    chore: initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,14 @@
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.10",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    }
+  }
+};
